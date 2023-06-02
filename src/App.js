e0fec91fcf55a668a59333b59fbf490214393f49
jastMan2 <aleev-02@inbox.ru>
2023-06-02 20:57:32 +0400

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ADD_CASH, GET_CASH } from '.';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state)
+  console.log(cash)
+  const addCash = () => {dispatch/* 1 */(ADD_CASH(10)/* 2 */)};
+  const getCash = () => {dispatch(GET_CASH(10))};
+  const [login, setLogin] = useState({login:'', password:'',})
+  const autorithize = () => {
+    if (!login.login && !login.password) return false
+    fetch ('http://localhost:5000/login', {body: JSON.stringify(login), method:'get'})
+    .then((response) => {console.log(response)})
+    .catch((err) => {console.log(err)})
+  }
+  console.log(login)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='btns'>
+        {/* <button onClick={addCash}>ADD_CASH</button>
+        <button onClick={getCash}>GET_CASH</button> */}
+        <input onInput={(e) => {
+            setLogin({...login, login: e.target.value})//e.target - input / value - innerInput
+          }}/>
+        <input onInput={(e) => {
+            setLogin({...login, password: e.target.value}) 
+          }}/>
+        <button onClick={autorithize}>autoraze</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+//1 -  функция изменения состояния
+//2 -  редюсер описывающий как будет изменяться состояние
--- src/index.js
+++ src/index.js
@@ -2,16 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore} from 'redux' //новый импорт createSore
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const defaultState = {
+  cash: 0,
+};
+
+const reducer = (state = defaultState, action) => {          //создаю редьюсер обьясняющий как будет изменяться состояние
+  switch (action.type){
+    case "ADD_CASH":
+      return {...state, cash: state.cash + action.payload}
+    case "GET_CASH":
+      return {...state, cash: state.cash - action.payload}
+
+    default:
+      return state
+  }
+};
+
+export const GET_CASH = (cash) => {
+  return({type:"GET_CASH", payload: cash})      //создаю функцию для описания того как будет изменяться состояние и данные для изменения
+};
+
+export const ADD_CASH = (cash) => {
+  return({type:"ADD_CASH", payload: cash})
+};
+
+const store = createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
